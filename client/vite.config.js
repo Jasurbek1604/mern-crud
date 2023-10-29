514c201e38871fd51a3498c4a63a7d1a54feffc0
Jasurbek <jasurdev1604@gmail.com>
2023-10-29 17:55:00 -0400

    MERN CRUD APPLICATION

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-crud-5y9l.onrender.com",
         secure: false,
       },
     },
